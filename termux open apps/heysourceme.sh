aa4bc2e41b0e5bef3edc8c106a0efff072bcd3ec
xthan <hackattendant@yahoo.com>
2025-09-19 22:40:12 -0400

    add cash app

--- termux open apps/heysourceme.sh
+++ termux open apps/heysourceme.sh
@@ -2,3 +2,4 @@
 alias acode="am start com.foxdebug.acode/com.foxdebug.acode.MainActivity && clear"
 alias calendar="termux-open-url https://calendar.google.com && clear"
 alias camera="am start com.motorola.camera3/com.motorola.camera.Camera && clear"
+alias cashapp="am start com.squareup.cash && clear"
