2efd0b010dda29c0773d13acde82d53b1818235b
xthan <hackattendant@yahoo.com>
2025-09-20 00:56:06 -0400

    add google chrome

--- termux open apps/heysourceme.sh
+++ termux open apps/heysourceme.sh
@@ -3,3 +3,4 @@ alias acode="am start com.foxdebug.acode/com.foxdebug.acode.MainActivity && clea
 alias calendar="termux-open-url https://calendar.google.com && clear"
 alias camera="am start com.motorola.camera3/com.motorola.camera.Camera && clear"
 alias cashapp="am start com.squareup.cash && clear"
+alias chrome="am start com.android.chrome/com.google.android.apps.chrome.Main && clear"
