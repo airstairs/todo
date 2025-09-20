37c9bc439a8d255d07489f2852232e8122592b8a
xthan <hackattendant@yahoo.com>
2025-09-19 21:00:14 -0400

    add clear

--- termux open apps/heysourceme.sh
+++ termux open apps/heysourceme.sh
@@ -1,3 +1,3 @@
 #apps list
 alias acode="am start com.foxdebug.acode/com.foxdebug.acode.MainActivity && clear"
-alias calendar="termux-open-url https://calendar.google.com"
+alias calendar="termux-open-url https://calendar.google.com && clear"
